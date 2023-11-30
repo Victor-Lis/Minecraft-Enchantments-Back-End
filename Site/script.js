71e39b94a68bf7a3d27f7b0e08ff6328820db462
Victor-Lis <victorlisbronzo1@gmail.com>
2023-11-30 03:06:29 -0300

    v2

--- Site/script.js
+++ Site/script.js
@@ -1,12 +1,12 @@
 let alphabet
 
-async function getAlphabet(){
+async function getAlphabet() {
 
     let data = await fetch(`http://localhost:4000/all`)
-    .then(res => res.json())
-    .catch(e => console.log(e))
+        .then(res => res.json())
+        .catch(e => console.log(e))
 
-    if(data){
+    if (data) {
 
         alphabet = data
         console.log(alphabet)
@@ -15,18 +15,18 @@ async function getAlphabet(){
 
 }
 
-async function getEncatamentos(){
+async function getEncatamentos() {
 
     document.querySelector("#enchantment-table").classList.add("spin")
 
-    setInterval(() => {document.querySelector("#enchantment-table").classList.remove("spin")}, 2500)
+    setInterval(() => { document.querySelector("#enchantment-table").classList.remove("spin") }, 2500)
 
     let inputValue = document.querySelector("input").value
     let data = await fetch(`http://localhost:4000/letter-to-enchantment?text=${inputValue}`)
-    .then(res => res.json())
-    .catch(e => console.log(e))
+        .then(res => res.json())
+        .catch(e => console.log(e))
 
-    if(data){
+    if (data) {
 
         document.querySelector(".encantamentos").innerHTML = ""
 
@@ -34,25 +34,30 @@ async function getEncatamentos(){
 
             document.querySelector(".encantamentos").innerHTML += `
                 <div class="card">
-                    <h3> ${inputValue[index] != " "? inputValue[index]: '-'} </h3>
+                    <h3> ${inputValue[index] != " " ? inputValue[index] : '-'} </h3>
                     <img src='${img}'>
                 </div>
             `
-            
+
         })
 
     }
 
-}   
-
-function letter(l){
-
-    console.log(l)
-
 }
 
-document.querySelector("#enchantment-table").addEventListener("dragstart", (e) => {  
+function letter(l) {
 
+    document.querySelector(".encantamentos").innerHTML += `
+    <div class="card">
+        <h3> ${l} </h3>
+    </div>
+    `
+}
+
+document.querySelector("#enchantment-table").addEventListener("dragstart", (e) => {
+
+    document.querySelector(".selectable-card").innerHTML = " "
+    document.querySelector(".encantamentos").innerHTML = " "
     let alphabetKeys = Object.keys(alphabet)
     alphabetKeys.map((letter) => {
 
@@ -60,14 +65,24 @@ document.querySelector("#enchantment-table").addEventListener("dragstart", (e) =
 
     })
 
-    if(document.querySelector("body").style.backgroundImage == `url("./Images/library.svg")`){
+    if (document.querySelector("body").style.backgroundImage == `url("./Images/library.svg")`) {
         document.querySelector(".selectable-card").style.display = "none"
         document.querySelector("input").style.display = "inherit"
         document.querySelector("body").style.backgroundImage = `url("./Images/minecraftbg.jpg")`
-    }else{
+    } else {
         document.querySelector(".selectable-card").style.display = "inherit"
         document.querySelector("input").style.display = "none"
         document.querySelector("body").style.backgroundImage = `url("./Images/library.svg")`
+    }
+
+})
+
+document.addEventListener('keydown', (e) => {
+
+    if (e.keyCode == 82) {
+        if (document.querySelector("body").style.backgroundImage == `url("./Images/library.svg")`) {
+            document.querySelector(".encantamentos").innerHTML = "<p></p>"
+        }
     }
 
 })
